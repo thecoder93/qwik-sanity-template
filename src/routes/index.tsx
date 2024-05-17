ffdebf7d56301577fb85f3789666fb50b1771226
Gianluca La Manna <gl.lamanna@gmail.com>
2024-05-17 14:40:32 +0200

    chore: formatted index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,36 +1,31 @@
-import {  component$, useTask$ } from "@builder.io/qwik";
-import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
-import { getPages } from "~/lib/sanityApi";
-
+import { component$, useTask$ } from '@builder.io/qwik';
+import { routeLoader$, type DocumentHead } from '@builder.io/qwik-city';
+import { getPages } from '~/lib/sanityApi';
 
 export const usePageLoader = routeLoader$(async () => {
-  return await getPages();
+	return await getPages();
 });
 
 export default component$(() => {
+	const t = usePageLoader();
 
-  const t = usePageLoader()
-
-  useTask$(async ({ track }) => {
+	useTask$(async ({ track }) => {
 		track(() => console.log('BLA BBLA BLA', JSON.stringify(t.value)));
-    
-  })
-  
-  return (
-    <>
-       <h1 class="text-3xl font-bold underline">
-      Hello World!
-    </h1>
-    </>
-  );
+	});
+
+	return (
+		<>
+			<h1 class="text-3xl font-bold underline">Hello World!</h1>
+		</>
+	);
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik - Sanity",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik Sanity template",
-    },
-  ],
+	title: 'Welcome to Qwik - Sanity',
+	meta: [
+		{
+			name: 'description',
+			content: 'Qwik Sanity template'
+		}
+	]
 };
